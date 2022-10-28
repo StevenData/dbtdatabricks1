3b7afef8472e181a2f6eee35306e6171c4306119
stevenshendata@gmail.com <stevenshendata@gmail.com>
2022-10-28 05:30:25 +0000

    updated

--- models/staging/retail/stg_loyalty_segments.sql
+++ models/staging/retail/stg_loyalty_segments.sql
@@ -7,8 +7,8 @@ staged as (
         loyalty_segment_id,
         loyalty_segment_description,
         unit_threshold,
-        to_date(from_unixtime(valid_from,'yyyy-MM-dd')) as valid_from_date, 
-        to_date(from_unixtime(valid_to,'yyyy-MM-dd')) as valid_to_date, 
+        to_date(from_unixtime(valid_from, 'yyyy-MM-dd')) as valid_from_date,
+        to_date(from_unixtime(valid_to, 'yyyy-MM-dd')) as valid_to_date
     from source
 )
  
